20d3edf000ee944d47ac273e5864e75a373d752f
dabbi919 <919ddb@gmail.com>
2024-04-08 20:03:54 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true
+});
+
+sr.reveal('.title', {delay: 200, origin: 'top'});
+sr.reveal('.video', {delay: 450, origin: 'top'});
